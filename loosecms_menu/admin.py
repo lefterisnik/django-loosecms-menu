000ef4044d5244fa5ac2cbbc800c6788f76a1016
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-15 13:28:54 +0300

    Fix queryset in admin.

--- loosecms_menu/admin.py
+++ loosecms_menu/admin.py
@@ -5,8 +5,13 @@ from .models import Menu
 
 class MenuAdmin(admin.ModelAdmin):
     list_filter = ('manager',)
-    list_display = ('title', 'page', 'href', 'order', 'published')
+    list_display = ('title', 'manager', 'page', 'href', 'order', 'published')
     list_editable = ('published', 'order')
     search_fields = ['title',]
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            kwargs["queryset"] = Menu.objects.filter(parent=None)
+        return super(MenuAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Menu, MenuAdmin)
